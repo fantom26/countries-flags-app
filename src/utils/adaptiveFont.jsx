c38ab70b27fcedc5378cb00f4f7f2f18f3bda97c
Yehor <kravtsovyehor@gmail.com>
2022-06-09 07:59:35 +0300

    Finished creating the function AdaptiveFont
    AdaptiveFont - function, that allows me adapt element's font-size

--- src/utils/adaptiveFont.jsx
+++ src/utils/adaptiveFont.jsx
@@ -6,13 +6,19 @@ const addMobSize = (pcSize, mobSize) =>
 
 export const AdaptiveFont = ({ pcSize, mobSize }) => css`
   @media (max-width: 767px) {
-    font-size: calc(
-      ${mobSize}px + ${addMobSize(pcSize, mobSize)} * ((100vw - 320px) / 1200))
+    font-size: clamp(
+      ${mobSize}px,
+      calc(
+        ${mobSize}px + ${addMobSize(pcSize, mobSize)} * ((100vw - 320px) / 1200)
+      ),
+      ${pcSize}px
     );
   }
   @media (min-width: 767px) {
-    font-size: calc(
-      ${mobSize}px + ((${pcSize} - ${mobSize}) * (100vw / 1200))
+    font-size: clamp(
+      ${mobSize}px,
+      calc(${mobSize}px + ((${pcSize} - ${mobSize}) * (100vw / 1200))),
+      ${pcSize}px
     );
   }
 `;
